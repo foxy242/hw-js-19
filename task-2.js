4d35de3190ab632ec67040ce70afa21c11992c5f
foxy242 <114094487+foxy242@users.noreply.github.com>
2024-02-07 16:34:13 +0200

    22

--- task-2.js
+++ task-2.js
@@ -1,4 +1,10 @@
+const titleElement = document.querySelector("article-title")
+const imgElement = document.querySelector("article-img")
 
+titleElement.textContent = "бонджур"
+imgElement.src = "https://www.meme-arsenal.com/memes/f5c9e52d686d1405e5ae14bcf6e87380.jpg"
+imgElement.alt = "бонджур"
+imgElement.width = 1000
 
 // Створити розмітку з заголовком та зображенням.
 // За допомогою JavaScript отримати доступ до зображення та змінити значення атрибута
